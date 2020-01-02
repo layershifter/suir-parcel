fbf3e5ca279cf1ed94de02f060afb0c21912bef6
Oleksandr Fediashov <olfedias@microsoft.com>
2020-01-02 12:38:51 +0100

    use FUI variables to disable unused variants use custom icons

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 import React from 'react'
-import { render } from 'react-dom'
-import { Button } from 'semantic-ui-react'
+import {render} from 'react-dom'
+import {Button} from 'semantic-ui-react'
 
-import 'fomantic-ui-css/components/button.css'
-import 'fomantic-ui-css/components/icon.css'
+import './semantic/dist/components/button.css'
+// import './icon.css'
+
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {library} from '@fortawesome/fontawesome-svg-core'
+import {faBook} from '@fortawesome/free-solid-svg-icons'
+
+library.add(faBook)
 
 const App = () => {
-    return <Button content="Foo" icon="book" />
+    return <Button content="Foo" icon={<FontAwesomeIcon className="icon" icon="book"/>}/>
 }
 
-render(<App />, document.getElementById('root'))
+render(<App/>, document.getElementById('root'))
